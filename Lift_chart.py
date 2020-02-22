ba7f7732f4a5458230d9cb42d6791f4f3682fe44
Naga Sri Harsha Akavarapu <39739377+sri201822@users.noreply.github.com>
2020-02-22 18:37:08 +0530

    Create Lift_chart.py

---
+++ Lift_chart.py
@@ -0,0 +1,13 @@
+# lift chart for regression
+%matplotlib inline
+import matplotlib.pyplot as plt
+def lift_chart(pred, y, sort=True):
+    t = pd.DataFrame({'pred': pred, 'y': y.flatten()})
+    if sort:
+        t.sort_values(by=['y'], inplace=True)
+    plt.plot(t['y'].tolist(), label='expected')
+    plt.plot(t['pred'].tolist(), label='prediction')
+    plt.ylabel('output')
+    plt.legend()
+    plt.show()
+lift_chart(pred.flatten(),y_test)
